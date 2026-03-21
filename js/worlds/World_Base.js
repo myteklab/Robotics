32b6391ce8c4fb38c888c4ae9b149a44a5c8c618
ssouders <ssouders@myteklab.com>
2026-03-20 23:35:44 -0400

    Add debug logs for ground dimension computation

--- js/worlds/World_Base.js
+++ js/worlds/World_Base.js
@@ -303,6 +303,15 @@ var World_Base = function() {
       img.onload = function() {
         self.processedOptions.groundLength = this.width / 10.0 * self.processedOptions.imageScale * self.processedOptions.uScale;
         self.processedOptions.groundWidth = this.height / 10.0 * self.processedOptions.imageScale * self.processedOptions.vScale;
+        console.log('[GROUND DEBUG] setOptions img.onload computed:', {
+          imgPixels: this.width + 'x' + this.height,
+          imageScale: self.processedOptions.imageScale,
+          typeofImageScale: typeof self.processedOptions.imageScale,
+          uScale: self.processedOptions.uScale,
+          vScale: self.processedOptions.vScale,
+          groundLength: self.processedOptions.groundLength,
+          groundWidth: self.processedOptions.groundWidth
+        });
 
         let xPos = self.processedOptions.groundLength / 2 - 12;
         let yPos = self.processedOptions.groundWidth / 2 - 12;
@@ -436,6 +445,15 @@ var World_Base = function() {
     }
 
     return new Promise(async function(resolve, reject) {
+      console.log('[GROUND DEBUG] load() called with processedOptions:', {
+        image: self.processedOptions.image,
+        imageScale: self.processedOptions.imageScale,
+        groundLength: self.processedOptions.groundLength,
+        groundWidth: self.processedOptions.groundWidth,
+        uScale: self.processedOptions.uScale,
+        vScale: self.processedOptions.vScale,
+        groundType: self.processedOptions.groundType
+      });
       var groundMat = new BABYLON.StandardMaterial('ground', scene);
       var groundTexture = new BABYLON.Texture(self.processedOptions.image, scene);
       groundMat.diffuseTexture = groundTexture;
